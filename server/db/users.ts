8fa9b5ac1b6792db4c03eff8d68a9e290d108d77
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-09-15 10:26:32 +1200

    Fixed - Was only adding a users auth0sub and email to database when using database function to add a user, now adds all user data needed using new interface

--- server/db/users.ts
+++ server/db/users.ts
@@ -1,13 +1,12 @@
 import db from './connection.ts'
+import { UserData } from '../../models/user.ts'
 
 export async function checkForUser(auth0Sub: string) {
   const user = await db('users').where({ auth0_sub: auth0Sub }).first()
   return user
 }
 
-export async function createUser(auth0Sub: string, email: string) {
-  const [newUser] = await db('users')
-    .insert({ auth0_sub: auth0Sub, email })
-    .returning('*')
+export async function createUser(userData: UserData) {
+  const [newUser] = await db('users').insert(userData).returning('*')
   return newUser
 }
