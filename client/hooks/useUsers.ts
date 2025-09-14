2a10712cf51c43e7fc4ed0dafcb56a94273971ce
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-09-15 10:59:11 +1200

    Added - Hook function to add a user to database

--- client/hooks/useUsers.ts
+++ client/hooks/useUsers.ts
@@ -1,5 +1,6 @@
-import { useQuery } from '@tanstack/react-query'
-import { getUserFromAutho0Sub } from '../apis/users.ts'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { addUser, getUserFromAutho0Sub } from '../apis/users.ts'
+import { UserData } from '../../models/user.ts'
 
 export function useGetUserAuth0Sub(auth0Sub: string) {
   const query = useQuery({
@@ -8,4 +9,17 @@ export function useGetUserAuth0Sub(auth0Sub: string) {
     enabled: !!auth0Sub,
   })
   return query
+}
+
+export function useAddUser() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async (user: UserData) => {
+      const newUser = addUser(user)
+      return newUser
+    },
+    onSuccess: (data, variables) => {
+      queryClient.invalidateQueries({ queryKey: ['user', variables.auth0Sub] })
+    },
+  })
 }
