a9c41f55220dc30bb7292ba91b3ee1ca2f7edfa3
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-09-15 10:54:26 +1200

    Added - Client side api function to add a user to the database

--- client/apis/users.ts
+++ client/apis/users.ts
@@ -1,8 +1,14 @@
 import * as request from 'superagent'
+import { UserData } from '../../models/user'
 
 const rootUrl = '/api/v1'
 
 export async function getUserFromAutho0Sub(auth0Sub: string) {
   const user = await request.get(rootUrl + `/users/auth0/${auth0Sub}`)
+  return user.body
+}
+
+export async function addUser(userData: UserData) {
+  const user = await request.post(rootUrl + '/users').send(userData)
   return user.body
 }
