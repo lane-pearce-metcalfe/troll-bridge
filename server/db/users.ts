279aaac362a65a233673cc7698b567f0df09208e
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-09-14 13:26:39 +1200

    Added - Database function to add a new user

--- server/db/users.ts
+++ server/db/users.ts
@@ -4,3 +4,10 @@ export async function checkForUser(auth0Sub: string) {
   const user = await db('users').where({ auth0_sub: auth0Sub }).first()
   return user
 }
+
+export async function createUser(auth0Sub: string, email: string) {
+  const [newUser] = await db('users')
+    .insert({ auth0_sub: auth0Sub, email })
+    .returning('*')
+  return newUser
+}
