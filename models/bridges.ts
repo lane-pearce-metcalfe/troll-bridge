9ba498b41d606c516fd7eee424aa1fd0322b5543
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-09-15 15:12:41 +1200

    Added - Model for bridge data

---
+++ models/bridges.ts
@@ -0,0 +1,14 @@
+export interface Bridge {
+  id: number
+  name: string
+  location: string
+  type: string
+  length: number
+  height: number
+  year_built: number
+  added_by: string
+  troll_owner: string
+  lat: number
+  lng: number
+  img_url: string
+}
