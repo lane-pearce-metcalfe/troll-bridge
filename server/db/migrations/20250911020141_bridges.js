a201a1beb830442b7b9ad3127a56ed70a0b5e9e9
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-09-15 12:11:08 +1200

    Updated - Bridge migration file to have more info and allow a user to 'own' it

--- server/db/migrations/20250911020141_bridges.js
+++ server/db/migrations/20250911020141_bridges.js
@@ -3,8 +3,20 @@
  */
 export async function up(knex) {
   return knex.schema.createTable('bridges', (table) => {
-    table.increments('id')
+    table.increments('id').primary()
     table.string('name')
+    table.string('location')
+    table.string('type')
+    table.integer('length')
+    table.integer('height')
+    table.integer('year_built')
+    table.string('added_by')
+    table.foreign('added_by').references('users.id').onDelete('SET NULL')
+    table.string('troll_owner')
+    table.foreign('troll_owner').references('users.id').onDelete('SET NULL')
+    table.integer('lat')
+    table.integer('lng')
+    table.string('image_url')
   })
 }
 
