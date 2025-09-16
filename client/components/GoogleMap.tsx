d1b7a006e75effc041bb28f8779b13fdc837be83
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-09-17 08:59:07 +1200

    Fixed - I was using center rather than defaultCenter so every render of the google map it would center back to the middle of the map

--- client/components/GoogleMap.tsx
+++ client/components/GoogleMap.tsx
@@ -11,8 +11,8 @@ export default function GoogleMap() {
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <Map
-        defaultZoom={2}
-        center={{ lat: 0, lng: 0 }}
+        defaultZoom={10}
+        defaultCenter={{ lat: -36.8485, lng: 174.7633 }}
         style={{ width: '100%', height: '400px' }}
         mapId={import.meta.env.VITE_MAP_ID}
       >
