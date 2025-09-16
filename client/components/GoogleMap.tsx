eb00d336fc74480a89efd03c2837052cb4956264
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-09-17 08:56:54 +1200

    Updated - Google map now places markers on where all the bridges positions are

--- client/components/GoogleMap.tsx
+++ client/components/GoogleMap.tsx
@@ -1,12 +1,28 @@
-import { APIProvider, Map } from '@vis.gl/react-google-maps'
+import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps'
+import { useBridges } from '../hooks/useBridges'
+import { BridgeData } from '../../models/bridges'
 export default function GoogleMap() {
+  const { data } = useBridges()
+
+  if (!data) {
+    return <div>Loading...</div>
+  }
+
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <Map
         defaultZoom={2}
         center={{ lat: 0, lng: 0 }}
         style={{ width: '100%', height: '400px' }}
-      ></Map>
+        mapId={import.meta.env.VITE_MAP_ID}
+      >
+        {data.map((bridge: BridgeData, index: number) => (
+          <AdvancedMarker
+            position={{ lat: bridge.lat, lng: bridge.lng }}
+            key={index}
+          />
+        ))}
+      </Map>
     </APIProvider>
   )
 }
